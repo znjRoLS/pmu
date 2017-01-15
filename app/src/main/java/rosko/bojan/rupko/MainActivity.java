b7f8b1710cab8418d7f15b3c3d463ea455db42bc
rols <znjrols5@gmail.com>
2017-01-15 18:57:11 +0100

    Work in progress

--- app/src/main/java/rosko/bojan/rupko/MainActivity.java
+++ app/src/main/java/rosko/bojan/rupko/MainActivity.java
@@ -1,18 +1,60 @@
 package rosko.bojan.rupko;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements Controller.ViewInterface {
+public class MainActivity extends AppCompatActivity implements Controller.ViewInterface, SensorEventListener {
+
+    Controller controller;
+    SensorManager sensorManager;
+    Sensor gameVectorSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        controller = new Controller(this);
+
+        setupGameVectorSensor();
+    }
+
+    private void setupGameVectorSensor() {
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        gameVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+
+        sensorManager.registerListener(this, gameVectorSensor,
+                SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
     public void updateView() {
         
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, gameVectorSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
